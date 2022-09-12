e1f256c668312d1d0c9c5340b669a96eaafc9a45
arpitpatel365 <90886271+arpitpatel365@users.noreply.github.com>
2022-09-12 16:34:36 +0530

    v 1.0
    Crud Operation in React.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import ToDoFunction from './ToDoFunction';
+
+
+function App() {
+  return (
+    <div className="App x">
+      <ToDoFunction/>
+    </div>
+  );
+}
+
+export default App;
+      
+      
+
+     
